5447a487e0dbabfe292774b90529fcbfeed36a82
nieaowei <nieaowei@gmail.com>
2019-09-27 01:07:32 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,5 +6,5 @@ systemctl enable docker
 systemctl start docker
 echo "The images is pulling."
 docker pull nieaowei/campus_network_unlock
-docker run -d -p 443:443 -p 53:53 -p 67:67 -p 68:68 -p 69:69 nieaowei/campus_network_unlock
+docker run -d -p 443:443 -p 53:53/udp -p 67:67/udp -p 68:68/udp -p 69:69/udp nieaowei/campus_network_unlock
 echo "The container is started."
