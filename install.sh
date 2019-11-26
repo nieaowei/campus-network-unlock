8c084a190ced18d3834c366a341f8fdf897df6e0
nieaowei <nieaowei@gmail.com>
2019-11-26 16:07:40 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,8 +10,9 @@ docker pull nieaowei/campus_network_unlock
 echo "The dchlient is closing. The port 68 is open for docker."
 ps -ef | grep dhclient | grep -v grep | awk '{print $2}' | xargs kill -9
 docker run --net=host -d nieaowei/campus_network_unlock
-mkdir ~/auto-script && cd ~/auto-script
-curl https://raw.githubusercontent.com/nieaowei/campus_network_unlock/master/campus_network_unlock.sh | bash
+mkdir ~/auto-scripts && cd ~/auto-scripts
+curl -O https://raw.githubusercontent.com/nieaowei/campus_network_unlock/master/campus_network_unlock.sh
+chmod +x campus_network_unlock.sh && ./campus_network_unlock.sh
 echo "Add auto start scripts."
 bash "echo \"~/auto-scripts/campus_network_unlock.sh\" >> /etc/rc.d/rc.local"
 chmod +x /etc/rc.d/rc.local
