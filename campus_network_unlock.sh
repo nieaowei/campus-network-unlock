2172ce07a66c8b7e6565cb3ea6d2caff8287a0d0
nieaowei <nieaowei@gmail.com>
2019-11-26 18:07:06 +0800

    Update campus_network_unlock.sh

--- campus_network_unlock.sh
+++ campus_network_unlock.sh
@@ -1,3 +1,14 @@
-echo "The campus_network_unlock is starting."
-docker run --net=host -d nieaowei/campus_network_unlock
-echo "The container is started."
+if ps -ef | grep dhclient | grep -v grep | awk '{print $2}' | xargs kill -9
+then
+	echo "The dhclient is closed."
+else
+	echo "The dhclent closeing is faild. It probably is closed."
+fi
+
+if docker run --net=host -d nieaowei/campus_network_unlock
+then
+	echo "The images is running."
+else
+	echo "The images running is faild."
+	exit 4
+fi
